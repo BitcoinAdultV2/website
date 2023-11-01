d826268ba4f5201e43c6a1451bd2c0c0c74ceccd
baal <baalb4605@gmail.com>
2023-11-01 18:34:44 +0100

    add color

--- app/wallet/layout.tsx
+++ app/wallet/layout.tsx
@@ -1,13 +1,95 @@
+import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/card";
+import { Image } from "@nextui-org/image";
+import { Divider } from "@nextui-org/divider";
+import { Link } from "@nextui-org/link";
 export default function DocsLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
 	return (
+		<>
+		<div className="base-color h-full">	
 		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
 			<div className="inline-block max-w-lg text-center justify-center">
 				{children}
 			</div>
 		</section>
+		<div className="flex flex-row flex-wrap justify-center items-center gap-4 w-full">
+		<Card className="max-w-[400px]">
+      <CardHeader className="flex gap-3">
+        <Image
+          alt="linux logo"
+          height={40}
+          radius="sm"
+          src="/linux.png"
+          width={40}
+        />
+        <div className="flex flex-col">
+          <p className="text-md">Linux</p>
+        </div>
+      </CardHeader>
+      <Divider/>
+      <CardFooter>
+        <Link
+          isExternal
+          showAnchorIcon
+          href="https://github.com/BitcoinAdultV2/BitcoinAdultV2/releases/download/v2.0.0/bitcoinadult2.0.0-x86_64-linux-gnu.tar.gz"
+        >
+          Download from GitHub.
+        </Link>
+      </CardFooter>
+    </Card>
+	<Card className="max-w-[400px]">
+      <CardHeader className="flex gap-3">
+        <Image
+          alt="Windows logo"
+          height={40}
+          radius="sm"
+          src="/windows.png"
+          width={40}
+        />
+        <div className="flex flex-col">
+          <p className="text-md">Windows</p>
+        </div>
+      </CardHeader>
+      <Divider/>
+      <CardFooter>
+        <Link
+          isExternal
+          showAnchorIcon
+          href="https://github.com/BitcoinAdultV2/BitcoinAdultV2/releases/download/v2.0.0/bitcoinadult2.0.0-win64.zip"
+        >
+          Download from GitHub.
+        </Link>
+      </CardFooter>
+    </Card>
+	<Card className="max-w-[400px]">
+      <CardHeader className="flex gap-3">
+        <Image
+          alt="Mac logo"
+          height={40}
+          radius="sm"
+          src="/mac.png"
+          width={40}
+        />
+        <div className="flex flex-col">
+          <p className="text-md">Mac</p>
+        </div>
+      </CardHeader>
+      <Divider/>
+      <CardFooter>
+        <Link
+          isExternal
+          showAnchorIcon
+          href="https://github.com/BitcoinAdultV2/BitcoinAdultV2/releases/download/v2.0.0/bitcoinadult2.0.0-x86_64-apple-darwin.tar.gz"
+        >
+          Download from GitHub.
+        </Link>
+      </CardFooter>
+    </Card>
+	   </div>
+	   </div>
+	   </>
 	);
 }
